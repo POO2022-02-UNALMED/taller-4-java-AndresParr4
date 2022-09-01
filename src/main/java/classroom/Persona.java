450a928edd12f520f3e86fcdfeac4854561c7b8d
AndresParr4 <aparran@unal.edu.co>
2022-08-31 19:20:15 -0500

    :D

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -1,48 +1,53 @@
 package classroom;
 
 public class Persona {
+	
+	final long cedula;
+	String nombre;
+	static int totalPersonas;
 
-    final long cedula;
-    String nombre;
-    static int totalPersonas;
-    
-    static {
-        totalPersonas = 0;
-        cedula = 3;
-    }
+	static {
+		totalPersonas = 0;
+	}
 
-    Persona(long cedula, String nombre) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        totalPersonas++;
-    }
-    
-    Persona(String nombre, long cedula) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        totalPersonas++;
-    }
+	Persona(long cedula, String nombre) {
+		this.cedula = cedula;
+		this.nombre = nombre;
+		totalPersonas++;
+	}
 
-    Persona(long cedula) {
-        this.cedula = cedula;
-        this.nombre = "";
-        totalPersonas++;
-    }
+	Persona(String nombre, long cedula) {
+		this.cedula = cedula;
+		this.nombre = nombre;
+		totalPersonas++;
+	}
 
-    Persona(String nombre) {
-        this.nombre = "";
-        totalPersonas++;
-    }
-    
-    long getCedula() {
-        return cedula;
-    }
+	Persona(long cedula) {
+		this.cedula = cedula;
+		this.nombre = "";
+		totalPersonas++;
+	}
 
-    String getNombre() {
-        return nombre;
-    }
+	Persona(String nombre) {
+		this.cedula = 1;
+		this.nombre = nombre;
+		totalPersonas++;
+	}
 
-    void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	Persona() {
+		this(0, "");
+	}
+
+	long getCedula() {
+		return cedula;
+	}
+
+	String getNombre() {
+		return nombre;
+	}
+
+	void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
 }
--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -1,12 +1,25 @@
 package classroom;
 
-public enum Tipo {
-	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
+public class Tipo {
 	
-	private int codigo;
-	private String nombre;
-	public Tipo(int codigo,String nombre) {
-		this.codigo = codigo;
-		this.nombre = nombre;
+	public enum tipo {
+		DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
+		
+		Tipo tipo;
+		private int codigo;
+		private String nombre;
+		
+		tipo(int codigo) {
+			
+			this.codigo = codigo;
+			//this.nombre = nombre;
+		}
+	
 	}
+
+	public static final Tipo FUNDAMENTACION = null;
+	public static final Tipo DISCIPLINAR = null;
+
 }
+
+	
